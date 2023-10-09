6647e89109e39172004d83ad7ecbfaded18e17ae
DDevil NightRaid <ddevil@gmail.com>
2023-10-09 15:39:37 +0545

    fixing some deployment issues

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,11 @@ import { getAveragePrice, getHighestPrice, getLowestPrice } from '@/lib/utils';
 import { connectToDB } from './../../../lib/mongoose';
 import { generateEmailBody, sendEmail } from '@/lib/nodemailer';
 import { NextResponse } from 'next/server';
+
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
     try {
         connectToDB();
@@ -33,7 +38,7 @@ export async function GET() {
                 }
 
                 const updatedProduct = await Product.findOneAndUpdate(
-                    { url: scrapedProduct.url },
+                    { url: product.url },
                     product,
                 );
 
